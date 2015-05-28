fd2e43996bd42d2719b8ac9e83af63a211d17720
tiru <tiru67@gmail.com>
2015-05-28 17:11:02 -0400

    version 2 - changes #3 -  navigation fixes

--- app/views/home/home-ctrl.js
+++ app/views/home/home-ctrl.js
@@ -5,17 +5,18 @@
 angular.module('fabech').controller('homeController',function($scope,$rootScope,$anchorScroll,$location,$timeout,$routeParams,$document){
     $rootScope.$on('$routeChangeSuccess', function(){
         $document.find('#nav-bar').removeClass('in');
-        $location.hash($routeParams.scrollTo);
-        //$anchorScroll.yOffset = 100;
-        $anchorScroll();
 
-        window.scrollBy(0,-95);
+            $location.hash($routeParams.scrollTo);
+            //$anchorScroll.yOffset = 100;
+            $anchorScroll();
+
+            window.scrollBy(0, -95);
 
     });
 
     $scope.goToPath = function(path){
+        window.location.hash = "#/"+path;
 
-        $location.path(path);
     };
 
 });
